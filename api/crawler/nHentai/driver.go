35fb3e70a639dc4b2cb591c9896c65bb6619605c
skb-ninja <taka6gi@gmail.com>
2021-09-30 22:09:54 +0900

    some fixed

--- api/crawler/nHentai/driver.go
+++ api/crawler/nHentai/driver.go
@@ -1,8 +1,59 @@
-package nHentai
+package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
 
-func getDriver() int {
-	fmt.Println()
-	return 0
+	"github.com/sclevine/agouti"
+)
+
+func getDriver() *agouti.WebDriver {
+	options := agouti.ChromeOptions(
+		"args", []string{
+			"--disable-gpu",
+		})
+	driver := agouti.ChromeDriver(options)
+
+	return driver
+}
+
+func getScanner() *bufio.Scanner {
+	return bufio.NewScanner(os.Stdin)
+}
+
+func makeArray() []string {
+	var s []string
+	sc := getScanner()
+	fmt.Println("makeArray() is Called!")
+	for {
+		sc.Scan()
+		var p string = sc.Text()
+		if p == "Q" {
+			break
+		}
+
+		p = strings.TrimSpace(p)
+		s = append(s, p)
+	}
+	return s
+}
+
+func main() {
+	driver := getDriver()
+	err := driver.Start()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	page, err := driver.NewPage()
+	if err != nil {
+		fmt.Println(err)
+	}
+	page.Navigate("https://google.com")
+	fmt.Println(page.Title())
+
+	defer driver.Stop()
+
 }
